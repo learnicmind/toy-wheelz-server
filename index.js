82b9be21ed88390ef275c7b21b807e09a3c86f78
Ariful Islam Razu <learnicmind@gmail.com>
2023-05-20 22:25:13 +0600

    alltoys data added to database

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hdichay.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,21 +28,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('carWheelz').collection('alltoys');
+
+    app.get('/alltoys', async (req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('toy wheelz is running')
+  res.send('toy wheelz is running')
 })
 
 app.listen(port, () => {
-    console.log(`Toy wheelsz server is running on the port: ${port}`)
+  console.log(`Toy wheelsz server is running on the port: ${port}`)
 })
