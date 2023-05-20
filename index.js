93cfa4e769c4e5ab9c331a051aae032c102a9fc2
Ariful Islam Razu <learnicmind@gmail.com>
2023-05-21 00:08:04 +0600

    single data get by id

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -33,6 +33,13 @@ async function run() {
 
     app.get('/alltoys', async (req, res) => {
       const result = await toyCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/alltoys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toyCollection.findOne(query);
       res.send(result)
     })
 
